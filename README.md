a9d5a74d064bb7fc322d04530b539ffca057af96
Remelens <109774335+Remelens@users.noreply.github.com>
2023-12-10 14:08:01 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+# 小张聊天室VSCode插件版
+
+---
+欢迎使用小张聊天室vscode插件，聊天室原站见 https://chat.zhangsoft.link/ 。  
+注意：在这里，我们把"房间（chatroom）"称作"频道（channel）"。  
+调用`zhangchat.join`命令以加入频道。  
+主频道（在线用户多）： `chat`  
+您也可以自己创建频道，只需要调用时输入频道名称即可。
+
+---
+本聊天室依照中华人民共和国相关法律，保存您的聊天记录。  
+无论您是否在中国境内，都请自觉遵守中华人民共和国相关法律和聊天室内相关规定。  
+本站坚决不欢迎盲目自大、言论弱智的人。  
+您如果对本聊天室不满意或认为受到不公平对待，则可以选择向管埋员申诉或选择离开。
+
+---
+您知道吗？这个聊天室原本是[MelonFish](https://gitee.com/XChatFish)交给[MrZhang365](https://blog.mrzhang365.cf)开发的XChat聊天室。  
+但是由于某些原因，它被开发者魔改成了现在的小张聊天室。
+XChat基于HackChat，HackChat的GitHub仓库地址为：https://github.com/hack-chat/main  
+小张聊天室的仓库地址为：https://github.com/ZhangChat-Dev-Group/ZhangChat  
+在此对HackChat的开发者深表感谢。
+
+---
+本聊天室开发者：  
+@MrZhang365 - [MrZhang365的博客](https://blog.zhangsoft.link/) && [小张软件](https://www.zhangsoft.link/)  
+@paperee - [纸片君ee的博客](https://blog.paperee.guru/)
+
+---
+更多代码贡献者：  
+@[4n0n4me](http://github.com/xjzh123/) - 编写了[hackchat\\+\\+客户端](https://hc.thz.cool/)  
+@[Dr0](https://github.com/redble) - 编写了[ZhangChat增强脚本](https://greasyfork.org/zh-CN/scripts/458989-zhchat%E5%A2%9E%E5%BC%BA%E8%84%9A%E6%9C%AC)
+
+---
+友情链接：  
+[HackChat聊天室](https://hack.chat/)  
+[hackchat\+\+客户端](https://hc.thz.cool/)  
+[IM模式客户端（By @Dr0）](https://im.chat.zhangsoft.link/)  
+[ZhangChat增强脚本](https://greasyfork.org/zh-CN/scripts/458989-zhchat%E5%A2%9E%E5%BC%BA%E8%84%9A%E6%9C%AC)
+
+---
+2023.02.23~2023.03.26 [小张聊天室开发组](https://githubfast.com/ZhangChat-Dev-Group) 致  
+**本站由[雨云](https://www.rainyun.com/MjcxMTc=_)提供计算服务**
---
+++ extension.js
@@ -0,0 +1,116 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const WebSocket = require("ws");
+let chat,onchat=false;
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	// 激活提示
+	console.log('Zhangchat running...');
+	// 命令在package.json里被定义
+	// 用registerCommans注册
+	// commandId 参数必须与 package.json 中的命令字段匹配
+	//join
+	let zhcjoin = vscode.commands.registerCommand("zhangchat.join",async function(){
+		if(onchat){
+			vscode.window.showErrorMessage('您已经加入频道!');
+		}
+		let channel=await vscode.window.showInputBox({ prompt: '请输入加入频道' }).then(keyword => {
+			return keyword;
+		});
+		let username=await vscode.window.showInputBox({ prompt: '请输入用户名' }).then(keyword => {
+			return keyword;
+		});
+		chat = zhc(channel, username);
+		onchat=true;
+	})
+	context.subscriptions.push(zhcjoin);
+	//send
+	let zhcsend = vscode.commands.registerCommand("zhangchat.send",async function(){
+		if(!onchat){
+			vscode.window.showErrorMessage('您未加入频道!');
+			await vscode.commands.executeCommand('zhangchat.join');
+		}
+		let texts=await vscode.window.showInputBox({ prompt: '发送消息：' }).then(keyword => {
+			return keyword;
+		});
+		chat.sendMessage(texts);
+	})
+	context.subscriptions.push(zhcsend);
+	//close
+	let zhcclose = vscode.commands.registerCommand("zhangchat.close",async function(){
+		if(!onchat){
+			vscode.window.showErrorMessage('您未加入频道!');
+			return;
+		}
+		chat.close();
+	})
+	context.subscriptions.push(zhcclose);
+}
+
+// 当您的扩展被停用时，将调用此方法
+function deactivate() {}
+
+function zhc(channel, myNick) {
+	const ws = new WebSocket('wss://chat.zhangsoft.link/ws');
+  	ws.on('open', function() {
+	    ws.send(JSON.stringify({ cmd: 'join', channel: channel, nick: myNick, client: 'ZHCvscode' }));
+  	});
+  	ws.onmessage=function(event) {
+    	var data=JSON.parse(event.data);
+		data.trip=(!data.trip?'NULL':data.trip);
+		if(data.cmd==="onlineSet"){
+			let res='';
+			for(let i=0;i<data.nicks.length;i++){
+				res+=data.nicks[i];
+				if(i!==data.nicks.length-1){
+					res+=',';
+				}
+			}
+			vscode.window.showInformationMessage(`在线用户: ${res}`);
+		}else if(data.cmd==="onlineRemove"){
+			vscode.window.showInformationMessage(`${data.nick} 离开了聊天室`);
+		}else if(data.cmd==="onlineAdd"){
+			vscode.window.showInformationMessage(`${data.nick}#${data.trip} 加入了聊天室
+Ta正在使用 ${data.client}`);
+		}else if(data.cmd==="chat"){
+			vscode.window.showInformationMessage(`${data.nick}#${data.trip}:${data.text}`);
+		}else if(data.cmd==="info"){
+			vscode.window.showInformationMessage(`提示${(!data.nick?'':"#"+data.nick)}:${data.text}`);
+		}else{
+			//console.log(event.data);//debug
+		}
+  	}
+  	ws.on('close', function() {
+		onchat=false;
+    	vscode.window.showInformationMessage(`ZhangChat断开连接`);
+  	});
+  	function sendMessage(text) {
+    	ws.send(JSON.stringify({ cmd: 'chat', text: text }));
+  	}
+	function sendInfo(text) {
+    	ws.send(JSON.stringify({ cmd: 'emote', text:text }));
+  	}
+	function send(json) {
+    	ws.send(JSON.stringify(json));
+  	}
+	function close() {
+    	ws.close();
+  	}
+  	return {
+    	sendInfo: sendInfo,
+    	sendMessage: sendMessage,
+		send:send,
+		close:close
+  	};
+}
+
+module.exports = {
+	activate,
+	deactivate
+}
